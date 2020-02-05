6a04cbb67dd93b1ebb593ae9a96d3b6f73b59947
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-02-04 19:13:33 -0800

    Moved files into folders that they belong in. Choosing tags turns off and on a checkmark in the tableview.

--- ZipR/ZipR/View Controllers/PostTagTableViewController.swift
+++ ZipR/ZipR/View Controllers/PostTagTableViewController.swift
@@ -15,10 +15,18 @@ protocol PostTagTableViewControllerDelegate {
 class PostTagTableViewController: UITableViewController {
 
     var delegate: PostTagTableViewControllerDelegate?
-    
+    var tags = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+
+    private func toggleTag(_ tagString: String) -> Bool {
+        if tags.contains(tagString) {
+            return true
+        }
+        return false
     }
 
     @IBAction private func doneButtonTapped(_ sender: UIBarButtonItem) {
@@ -42,17 +50,25 @@ class PostTagTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TagCell", for: indexPath)
 
-        cell.textLabel?.text = Tag.allCases[indexPath.row].rawValue.capitalized
+        let tagString = Tag.allCases[indexPath.row].rawValue
+        cell.textLabel?.text = tagString.capitalized
+        cell.imageView?.image = toggleTag(tagString) ? UIImage(systemName: "checkmark") : nil
         //cell.imageView?.image = UIImage(systemName: "checkmark")
 
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TagCell", for: indexPath)
 
-        //cell.imageView?.image = UIImage(systemName: "checkmark")
-
+        let tagString = Tag.allCases[indexPath.row].rawValue
+        if toggleTag(tagString) {
+            if let index = tags.firstIndex(of: tagString) {
+                tags.remove(at: index)
+            }
+        } else {
+            tags.append(tagString)
+        }
+        tableView.reloadData()
     }
 
 
