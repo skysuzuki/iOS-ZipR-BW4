f77ed4b56d86b817daefee915b16e8e912927225
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2020-02-06 20:51:36 -0500

    Delete post function now working

--- ZipR/ZipR/View Controllers/MyRecentActivtyTableViewController.swift
+++ ZipR/ZipR/View Controllers/MyRecentActivtyTableViewController.swift
@@ -56,50 +56,13 @@ class MyRecentActivtyTableViewController: UITableViewController {
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let idString = usersPosts[indexPath.row].id
+            usersPosts = usersPosts.filter {$0.id != idString}
+            postController?.deletePostandItsComments(postId: idString ?? "")
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "Deletion"), object: self)
+            tableView.reloadData()
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
